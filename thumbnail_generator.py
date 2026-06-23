4927c8abbb3e7c6aba8a8ee4e1a20f4d54c0557f
naincysoni499-svg <naincysoni499@gmail.com>
2026-06-23 09:51:45 +0530

    Improved thumbnail generation script

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -2,13 +2,15 @@ import subprocess
 
 video_file = "Uploads/sample.mp4"
 thumbnail_file = "thumbnail.jpg"
-print(video_file)
+
+print("Generating thumbnail...")
+
 subprocess.run([
     "ffmpeg",
     "-i", video_file,
     "-ss", "00:00:05",
-    "-vframes", "1",
+    "-frames:v", "1",
     thumbnail_file
 ])
 
-print("Thumbnail generated!")
+print("Thumbnail generated successfully!")
